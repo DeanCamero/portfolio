b707b83e84d93c4ac96b51ce557798d7156c646c
DeanCamero <camerodean1@gmail.com>
2025-05-08 13:18:21 +0800

    Add files via upload

---
+++ sounds/ScrollTrigger.js
@@ -0,0 +1,16 @@
+/*
+ * Skypack CDN - gsap@3.13.0
+ *
+ * Learn more:
+ *   📙 Package Documentation: https://www.skypack.dev/view/gsap
+ *   📘 Skypack Documentation: https://www.skypack.dev/docs
+ *
+ * Pinned URL: (Optimized for Production)
+ *   ▶️ Normal: https://cdn.skypack.dev/pin/gsap@v3.13.0-ZeKZLz8vbDmhQ7irSiFF/mode=imports/unoptimized/ScrollTrigger.js
+ *   ⏩ Minified: https://cdn.skypack.dev/pin/gsap@v3.13.0-ZeKZLz8vbDmhQ7irSiFF/mode=imports,min/unoptimized/ScrollTrigger.js
+ *
+ */
+
+// Browser-Optimized Imports (Don't directly import the URLs below in your application!)
+export * from '/-/gsap@v3.13.0-ZeKZLz8vbDmhQ7irSiFF/dist=es2019,mode=imports/unoptimized/ScrollTrigger.js';
+export {default} from '/-/gsap@v3.13.0-ZeKZLz8vbDmhQ7irSiFF/dist=es2019,mode=imports/unoptimized/ScrollTrigger.js';
